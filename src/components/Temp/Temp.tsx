036751188983ed4b0a1b9dfa51d9f75a8ba6974b
Amankumar Sadhwani <amankumar_sadhwani@epam.com>
2022-06-14 09:50:02 +0530

    bug fixes

--- src/components/Temp/Temp.tsx
+++ src/components/Temp/Temp.tsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export function Temp() {
-  const [data, setData] = React.useState({
-    test: "hello",
-  });
+  const [counter, setCounter] = useState(1200);
+  function handleCounter() {
+    setCounter((prevCount) => prevCount + 1);
+  }
+  useEffect(() => {
+    const timeInterval = setInterval(() => {
+      counter < 1202 && setCounter((prevCount) => prevCount + 1);
+    }, 2000);
 
-  const change = (e: any) => {
-    data.test = e.target.value;
-    debugger;
-    setData(data);
-  };
-
-  return <input value={data.test} onChange={change}></input>;
+    return () => {
+      clearInterval(timeInterval);
+    };
+  }, [counter]);
+  return <h1>{counter} </h1>;
 }
